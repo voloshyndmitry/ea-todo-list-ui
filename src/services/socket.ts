8bc36808027fcf4fe47e134ecfef9283027be09d
Dmitry Voloshin <woloshindima@gmail.com>
2023-08-01 00:45:55 -0600

    fix-websockets

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
 const URL = "ea-todo-list-api-6751d356280b.herokuapp.com";
-export const socket = io(`ws://${URL}`, { transports: ["websocket"] });
+export const socket = io(`wss://${URL}`, { transports: ["websocket"] });
